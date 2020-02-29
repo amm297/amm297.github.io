a0d61809d6c3881d5e2b9c08f0bfe0516151e3de
Alberto <alberto.martinez.martinez.1@gmail.com>
2020-02-29 13:40:22 +0100

    Added map v1

---
+++ js/map.js
@@ -0,0 +1,13 @@
+const provider =
+  'http://{s}.sm.mapstack.stamen.com/(toner-lite,$fff[difference],$fff[@23],$fff[hsl-saturation@20])/{z}/{x}/{y}.png';
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const width = 800;
+const height = 400;
+const center = [40.416775, -3.70379];
+
+const map = L.map('map').setView(center, 7);
+L.tileLayer(provider, { attribution }).addTo(map);
+
+d3.csv('/resources/airbnb-listings.csv', rows => console.log(rows));
